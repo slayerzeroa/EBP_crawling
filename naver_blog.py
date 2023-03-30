5d9e06372b8de52b6cb8485ef5f7a7a0dfcc8920
slayerzeroa <slayerzeroa@naver.com>
2023-03-30 02:28:27 +0000

    ydm_230330

--- naver_blog.py
+++ naver_blog.py
@@ -1,133 +1,141 @@
-'''
-Author : slayerzeroa
+# '''
+# Author : slayerzeroa
 
-Date : 2022-09-26
+# Date : 2022-09-26
 
-네이버 블로그 포스팅 자동화
-'''
+# 네이버 블로그 포스팅 자동화
+# '''
 
-#pip install gensim==3.8.3
-#pip install webdriver_manager
-#pip install -U pyautoit
+# #pip install gensim==3.8.3
+# #pip install webdriver_manager
+# #pip install -U pyautoit
+# #pip install python-docx
 
-
-import requests
-from bs4 import BeautifulSoup as BS
-from gensim.summarization.summarizer import summarize
-import docx
-from datetime import date
-import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.action_chains import ActionChains
-import pyperclip
-import time
-from translate_content import start
-import autoit
-import os
-
-# 네이버 컨텐츠 마련
-translate_contents, translate_title = start()
-print(translate_title)
-print(translate_contents)
-
-# 네이버 자동 로그인
-
-id = ''
-password = ''
-
-# 네이버 자동 로그인 가정
-
-# 셀레니움
-driver_1 = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-action = ActionChains(driver_1)
-
-# 1. 네이버 접속
-main_url = "http://naver.com"
-blog_url = "https://blog.naver.com/slayerzeroa"
-driver_1.get(main_url)
-
-# 2. 로그인 버튼 클릭
-elem = driver_1.find_element(By.CLASS_NAME, 'link_login')
-elem.click()
-
-# 3. id 복사 붙여넣기
-elem_id = driver_1.find_element(By.ID, 'id')
-elem_id.click()
-pyperclip.copy(id)
-elem_id.send_keys(Keys.CONTROL, 'v')
-time.sleep(1)
-
-# 4. pw 복사 붙여넣기
-elem_pw = driver_1.find_element(By.ID, 'pw')
-elem_pw.click()
-pyperclip.copy(password)
-elem_pw.send_keys(Keys.CONTROL, 'v')
-time.sleep(1)
-
-# 5. 로그인 버튼 클릭
-driver_1.find_element(By.ID, 'log.login').click()
-time.sleep(10)
-
-# 6. 블로그 접속
-driver_1.get(blog_url)
-# 프레임 이동
-driver_1.switch_to.frame("mainFrame")
-
-# 7. 글쓰기 버튼 클릭
-elem = driver_1.find_element(By.XPATH, '//*[@id="post-admin"]/a[1]')
-elem.click()
-time.sleep(5)
-
-# 8-1. 사진 첨부
-driver_1.find_element(By.XPATH, '//button[contains(@class,"se-")]').click()
-time.sleep(5)
-handle = "[CLASS:#32770; TITLE:열기]"
-time.sleep(5)
-autoit.win_active("열기")
-time.sleep(5)
-img_path = "C:\\Users\\PSYDUCK\\PycharmProjects\\EBP_crawling\\pictures\\logo.PNG"
-autoit.control_send(handle, "Edit1", img_path)
-time.sleep(5)
-autoit.control_click(handle, "Button1")
-time.sleep(5)
-
-# 8. 내용 작성
-action.send_keys("\n").perform()
-time.sleep(5)
-action.send_keys(translate_contents).perform()
-time.sleep(5)
-action.send_keys("\n").perform()
-time.sleep(5)
-
-# 9. 제목 작성
-driver_1.find_element(By.XPATH, "//span[text()='제목']").click()
-time.sleep(5)
-action.send_keys(translate_title).perform()
-time.sleep(5)
+# import requests
+# from bs4 import BeautifulSoup as BS
+# from gensim.summarization.summarizer import summarize
+# import docx
+# from datetime import date
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.keys import Keys
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.common.action_chains import ActionChains
+# import pyperclip
+# import time
+# from translate_content import start
+# import os
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common.keys import Keys
+# import clipboard
 
 
-# 잡다한 sub panel 끄기
-driver_1.find_element(By.CLASS_NAME, "se-help-panel-close-button").click()
-time.sleep(5)
+# options = Options()
+# options.add_argument("--headless")
+# options.add_argument("window-size=1400,1500")
 
-# 10. 발행 버튼 클릭
-driver_1.find_element(By.XPATH, "//span[text()='발행']").click()
-time.sleep(2)
+# # 네이버 컨텐츠 마련
+# # translate_contents, translate_title = start()
+# # print(translate_title)
+# # print(translate_contents)
 
-# 11. 카테고리 버튼 클릭
-driver_1.find_element(By.XPATH, "//span[text()='골아픈 퀀트']").click()
-time.sleep(2)
+# translate_contents = "123"
+# translate_title = "1"
 
-# 12. 번역 카테고리 선택
-driver_1.find_element(By.XPATH, "//span[text()='번역']").click()
-time.sleep(2)
+# # 네이버 자동 로그인
 
-# 13. 발행
-driver_1.find_element(By.CLASS_NAME, "confirm_btn__Dv9du").click()
-time.sleep(10)
+# id = 'xds12345'
+# password = 'wheres!234'
 
-driver_1.close()
+
+# # 네이버 자동 로그인 가정
+
+# # 셀레니움
+# driver_1 = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+# #driver_1 = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+# action = ActionChains(driver_1)
+
+# # 1. 네이버 접속
+# main_url = "http://naver.com"
+# blog_url = "https://blog.naver.com/gorapa_"
+# driver_1.get(main_url)
+
+# # 2. 로그인 버튼 클릭
+# elem = driver_1.find_element(By.CLASS_NAME, 'link_login')
+# elem.click()
+
+
+# # 3. id 복사 붙여넣기
+# loginID = 'xds12345'
+# clipboard.copy(loginID)
+# driver_1.find_element(By.XPATH, '//*[@id="id"]').send_keys(Keys.CONTROL, 'v')
+
+# # 4. pw 복사 붙여넣기
+# loginPW = 'cnnbbc505505'
+# clipboard.copy(loginPW)
+# driver_1.find_element(By.XPATH, '//*[@id="pw"]').send_keys(Keys.CONTROL, 'v')
+# time.sleep(1)
+
+# # 5. 로그인 버튼 클릭
+# driver_1.find_element(By.ID, 'log.login').click()
+# time.sleep(10)
+
+# url = 'https://naver.com/'
+# driver_1.get(url)
+# text = driver_1.find_element(By.CLASS_NAME, 'email MY_EMAIL').text
+# print(text)
+
+# # 6. 블로그 접속
+# driver_1.get(blog_url)
+# # 프레임 이동
+# driver_1.switch_to.frame("mainFrame")
+# time.sleep(5)
+
+# # 7. 글쓰기 버튼 클릭
+# elem = driver_1.find_element(By.XPATH, '//*[@id="post-admin"]/a[1]')
+# elem.click()
+# time.sleep(5)
+
+# # # 8-1. 사진 첨부
+# # driver_1.find_element(By.XPATH, '//button[contains(@class,"se-")]').click()
+# # time.sleep(5)
+# # handle = "[CLASS:#32770; TITLE:열기]"
+# # time.sleep(5)
+
+# # 8. 내용 작성
+# action.send_keys("\n").perform()
+# time.sleep(5)
+# action.send_keys(translate_contents).perform()
+# time.sleep(5)
+# action.send_keys("\n").perform()
+# time.sleep(5)
+
+# # 9. 제목 작성
+# driver_1.find_element(By.XPATH, "//span[text()='제목']").click()
+# time.sleep(5)
+# action.send_keys(translate_title).perform()
+# time.sleep(5)
+
+
+# # 잡다한 sub panel 끄기
+# driver_1.find_element(By.CLASS_NAME, "se-help-panel-close-button").click()
+# time.sleep(5)
+
+# # 10. 발행 버튼 클릭
+# driver_1.find_element(By.XPATH, "//span[text()='발행']").click()
+# time.sleep(2)
+
+# # 11. 카테고리 버튼 클릭
+# driver_1.find_element(By.XPATH, "//span[text()='뉴스번역']").click()
+# time.sleep(2)
+
+# # # 12. 번역 카테고리 선택
+# # driver_1.find_element(By.XPATH, "//span[text()='번역']").click()
+# # time.sleep(2)
+
+# # 13. 발행
+# driver_1.find_element(By.CLASS_NAME, "confirm_btn__Dv9du").click()
+# time.sleep(10)
+
+# driver_1.close()
--- translate_content.py
+++ translate_content.py
@@ -1,92 +1,99 @@
-'''
-Author : slayerzeroa
+# '''
+# Author : slayerzeroa
 
-Date : 2022-09-26
+# Date : 2022-09-26
 
-네이버 블로그 포스팅 자동화
-'''
+# 네이버 블로그 포스팅 자동화
+# '''
 
-#pip install gensim==3.8.3
-#pip install webdriver_manager
+# #pip install gensim==3.8.3
+# #pip install webdriver_manager
 
-import requests
-from bs4 import BeautifulSoup as BS
-from gensim.summarization.summarizer import summarize
-import docx
-from datetime import date
-import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import time
+# import requests
+# from bs4 import BeautifulSoup as BS
+# from gensim.summarization.summarizer import summarize
+# import docx
+# from datetime import date
+# import os
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# import time
+# from selenium.webdriver.chrome.options import Options
 
-# 셀레니움
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.implicitly_wait(3)
 
-# link list
-# link-title 딕셔너리
-link_list = []
-link_title = {}
+# options = Options()
+# options.add_argument("--headless")
+# options.add_argument("window-size=1400,1500")
 
-def start():
-    # CNN 비즈니스 탭 링크
-    main_url = 'https://edition.cnn.com/business'
-    time.sleep(3)
-    driver.get(main_url)
-    html = driver.page_source
-    soup = BS(html, 'html.parser')
-    tags = soup.select('#us-zone-1 > div.l-container > div > div.column.zn__column--idx-1 > ul > li:nth-of-type(1) > article > div > div.cd__content > h3')[0].find_all('a')
-    for tag in tags:
-        link_list.append(tag['href'])
-        link_title[tag['href']] = tag.text  # 링크-타이틀 매칭
 
-    headline_link = 'https://edition.cnn.com'+link_list[0]
+# # 셀레니움
+# driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+# driver.implicitly_wait(3)
 
-    webpage = requests.get(headline_link)  # web
-    soup = BS(webpage.content, "html.parser")
-    time.sleep(3)
-    # CNN 본문내용 html class
-    data = soup.find_all(class_= "zn-body__paragraph")
-    data_text_all = ""
+# # link list
+# # link-title 딕셔너리
+# link_list = []
+# link_title = {}
 
-    title = soup.find("h1", class_="pg-headline")
+# def start():
+#     # CNN 비즈니스 탭 링크
+#     main_url = 'https://edition.cnn.com/business'
+#     time.sleep(3)
+#     driver.get(main_url)
+#     html = driver.page_source
+#     soup = BS(html, 'html.parser')
+#     tags = soup.select('.stack')[0].find_all('a')
+#     for tag in tags:
+#         link_list.append(tag['href'])
+#         link_title[tag['href']] = tag.text  # 링크-타이틀 매칭
 
-    if title == None:
-        print('using requests is failed. \n try 2nd')
-        data = soup.find_all(class_="paragraph inline-placeholder")
-        data_text_all = ""
-        title = soup.find("h1", class_="headline__text inline-placeholder")
+#     headline_link = link_list[0]
 
-    title_text = title.get_text()
-    title_text = title_text.lstrip()
-    # html 데이터에서 본문 텍스트만 뽑아오기
-    for data_text in data:
-      data_text = data_text.get_text()
-      data_text_all = data_text_all + data_text
+#     webpage = requests.get(headline_link)  # web
+#     soup = BS(webpage.content, "html.parser")
+#     time.sleep(3)
+#     # CNN 본문내용 html class
+#     data = soup.find_all(class_= "zn-body__paragraph")
+#     data_text_all = ""
 
-    # gensim summarize
-    news_summarize = summarize(data_text_all, ratio=0.2, word_count = 350)
+#     title = soup.find("h1", class_="pg-headline")
 
-    # Papago API
-    request_url = "https://openapi.naver.com/v1/papago/n2mt" #네이버 papago open api 사용
-    headers = {"X-Naver-Client-Id": "Q7UvwtjKdBk4K0X_ob36", "X-Naver-Client-Secret": "5l89mtr94u"} # id, secret id
-    params = {"source": "en", "target": "ko", "text": news_summarize} # 언어 Domain, Codomain, Text
-    response = requests.post(request_url, headers=headers, data=params)
+#     if title == None:
+#         print('using requests is failed. \n try 2nd')
+#         data = soup.find_all(class_="paragraph inline-placeholder")
+#         data_text_all = ""
+#         title = soup.find("h1", class_="headline__text inline-placeholder")
 
-    # API 오류 나면 이곳에서 확인
-    result = response.json()
-    translate_result = result['message']['result']['translatedText']
+#     title_text = title.get_text()
+#     title_text = title_text.lstrip()
+#     # html 데이터에서 본문 텍스트만 뽑아오기
+#     for data_text in data:
+#       data_text = data_text.get_text()
+#       data_text_all = data_text_all + data_text
 
-    params = {"source": "en", "target": "ko", "text": title_text} # 언어 Domain, Codomain, Text
-    response = requests.post(request_url, headers=headers, data=params)
-    result = response.json()
-    translate_title = result['message']['result']['translatedText']
+#     # gensim summarize
+#     news_summarize = summarize(data_text_all, ratio=0.2, word_count = 350)
 
-    translate_result = translate_result + '\n' + headline_link + '\n' + '\n#CNN ' +'#EBP '+'#번역'
-    translate_title = '[CNN] ' + translate_title
+#     # Papago API
+#     request_url = "https://openapi.naver.com/v1/papago/n2mt" #네이버 papago open api 사용
+#     headers = {"X-Naver-Client-Id": "uum4phxUF76z3cxrobGq", "X-Naver-Client-Secret": "dFS_xqRd3d"} # id, secret id
+#     params = {"source": "en", "target": "ko", "text": news_summarize} # 언어 Domain, Codomain, Text
+#     response = requests.post(request_url, headers=headers, data=params)
 
-    driver.close()
+#     # API 오류 나면 이곳에서 확인
+#     result = response.json()
+#     translate_result = result['message']['result']['translatedText']
 
-    return(translate_result, translate_title)
+#     params = {"source": "en", "target": "ko", "text": title_text} # 언어 Domain, Codomain, Text
+#     response = requests.post(request_url, headers=headers, data=params)
+#     result = response.json()
+#     translate_title = result['message']['result']['translatedText']
+
+#     translate_result = translate_result + '\n' + headline_link + '\n' + '\n#CNN ' +'#EBP '+'#번역'
+#     translate_title = '[CNN] ' + translate_title
+
+#     driver.close()
+
+#     return(translate_result, translate_title)
